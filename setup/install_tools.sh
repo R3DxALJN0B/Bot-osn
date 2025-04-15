e7fda0a1e5a25709711ff197d4c6f89dd1daa5cb
R3DxALJN0B <kkuu2030kku@gmail.com>
2025-04-15 11:38:49 +0300

    Add files via upload

--- setup/install_tools.sh
+++ setup/install_tools.sh
@@ -1,1 +1,25 @@
-# install_tools.sh - dummy content
+#!/bin/bash
+
+# تثبيت sherlock
+if [ ! -d "sherlock" ]; then
+    git clone https://github.com/sherlock-project/sherlock.git
+    cd sherlock
+    pip install -r requirements.txt
+    cd ..
+fi
+
+# تثبيت theHarvester
+if [ ! -d "theHarvester" ]; then
+    git clone https://github.com/laramies/theHarvester.git
+    cd theHarvester
+    pip install -r requirements/base.txt
+    cd ..
+fi
+
+# تثبيت PhoneInfoga
+if [ ! -d "PhoneInfoga" ]; then
+    git clone https://github.com/sundowndev/phoneinfoga.git
+    cd phoneinfoga
+    pip install -r requirements.txt
+    cd ..
+fi
